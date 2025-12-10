e4245ffafd07b057ecea4a46f020032838eec540
Zhiyu <2187669042@qq.com>
2025-12-10 19:09:49 +0800

    update password

--- frontend/src/pages/PredictPage.tsx
+++ frontend/src/pages/PredictPage.tsx
@@ -27,7 +27,7 @@ interface PredictFormValues {
   distance_to_metro_km: number;
 }
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const PredictPage: React.FC = () => {
   const [predictForm] = Form.useForm<PredictFormValues>();
